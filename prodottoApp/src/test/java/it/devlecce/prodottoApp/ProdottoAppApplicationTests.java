af2b01eb53b609f54e7019ce24e78e242fce4028
ygarrisi <ygarrisi@studenti.apuliadigitalmaker.it>
2022-04-07 11:59:45 +0200

    metodi testati e funzionanti

---
+++ prodottoApp/src/test/java/it/devlecce/prodottoApp/ProdottoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.devlecce.prodottoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdottoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
